904d239c9cb8ce9c43baf6efe5575060c879dd76
Benjamin Elfen <benjamin.elfen@boehringer-ingelheim.com>
2023-01-06 16:53:22 +0100

    changed pretter config

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,8 @@
-module.exports = require("@boehringer-ingelheim/prettier-config");
+module.exports = require('@boehringer-ingelheim/prettier-config');
 
 module.exports = {
-  ...require("@boehringer-ingelheim/prettier-config"),
-  printWidth: 140,
+  ...require('@boehringer-ingelheim/prettier-config'),
+  singleQuote: true,
+  tabWidth: 2,
+  useTabs: false,
 };
