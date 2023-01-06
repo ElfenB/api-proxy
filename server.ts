dba36335d8a1b11551add388d480ee310d2a553e
Benjamin Elfen <benjamin.elfen@boehringer-ingelheim.com>
2023-01-06 16:47:30 +0100

    added proxy initially

--- server.ts
+++ server.ts
@@ -1,17 +1,20 @@
-import dotenv from "dotenv";
-import express, { Express, Request, Response } from "express";
+import dotenv from 'dotenv';
+import express, { Express, Request, Response } from 'express';
+import proxy from 'express-http-proxy';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
 
-console.log(port)
+console.log(port);
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
+app.get('/', (req: Request, res: Response) => {
+  res.send('Express + TypeScript Server');
 });
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
+
+app.use('/proxy', proxy('www.google.de'));
